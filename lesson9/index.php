3d0238bc1d1f6d8c6bdb8e3662e088959ae8c46a
Dmitry <ngdude@mail.ru>
2016-11-26 22:29:44 +0300

    lesson 9 up

--- lesson9/index.php
+++ lesson9/index.php
@@ -1,20 +1,9 @@
 <?php
   require_once "class1.php";
 
-echo "</br>";
 $news1->showProfile();
-echo "</br>";
 $news2->showProfile();
-echo "</br>";
 $news3->showProfile();
-echo "</br>";
 $news4->showProfile();
-echo "</br>";
 $news5->showProfile();
-echo "</br>";
-
-
-var_dump(get_class_vars());
-var_dump(get_class_vars("NewsComments"));
-
 ?>
